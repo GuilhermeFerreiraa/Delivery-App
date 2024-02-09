b8b249e0c4ca0f50dac737af6a9d69c4859cccb4
teste-agua-edu <gui.2001@hotmail.com>
2024-02-09 07:19:52 -0300

    feat: list all items from data

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,77 @@
-import CategoryButton from '@/components/category-button';
-import Header from '@/components/header';
-import { View, FlatList } from 'react-native';
-import { CATEGORIES } from "@/utils/data/products";
-import { useState } from 'react';
+import CategoryButton from "@/components/category-button";
+import Header from "@/components/header";
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import Product from "@/components/product";
+import { Link } from "expo-router";
+import { useCartStore } from "@/stores/cart-store";
 
 export default function Home() {
- const [seletedCategory, setSeletedCategory] = useState<string>(CATEGORIES[0]);
+  const cartStore = useCartStore();
+  const [category, setCategory] = useState<string>(CATEGORIES[0]);
+  const sectionlistRef = useRef<SectionList>(null);
 
- const handleCategorySelected = (seletedCategory: string) => {
-  setSeletedCategory(seletedCategory);
- }
+  const cartQuantityItems = cartStore.products.reduce(
+    (total, product) => total + product.quantity,
+    0
+  );
 
- return (
-  <View className="flex-1 pt-8">
-   <Header title="Faça seu pedido" cartQuantity={10} />
+  const handleCategorySelected = (seletedCategory: string) => {
+    setCategory(seletedCategory);
 
-   <FlatList
-    data={CATEGORIES}
-    keyExtractor={(i) => i}
-    horizontal
-    className='max-h-10 mt-5'
-    contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
-    showsHorizontalScrollIndicator={false}
-    renderItem={({ item }) => <CategoryButton title={item} isSelected={item === seletedCategory} onPress={() => handleCategorySelected(item)} />}
-   />
-  </View>
- )
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === seletedCategory
+    );
+
+    if (sectionlistRef.current) {
+      sectionlistRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      });
+    }
+  };
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantity={cartQuantityItems} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(i) => i}
+        horizontal
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelected(item)}
+          />
+        )}
+      />
+
+      <SectionList
+        ref={sectionlistRef}
+        sections={MENU}
+        keyExtractor={(i) => i.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
+    </View>
+  );
 }
